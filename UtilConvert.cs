8af6ddf8e380fc3db70de7fdd791ca2d3331aff2
chengyb <chenyanbo1024@gmail.com>
2020-05-27 15:31:13 +0800

    add utilConvert.cs

---
+++ UtilConvert.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Core.Common.Helper
+{
+    /// <summary>
+    /// 工具类 - 类型转换
+    /// </summary>
+    public static class UtilConvert
+    {
+        #region Convert To Int
+
+        public static int ObjToInt(this object thisValue)
+        {
+            int reval = 0;
+            if (thisValue == null) return 0;
+            if (thisValue != null && thisValue != DBNull.Value && int.TryParse(thisValue.ToString(), out reval))
+            {
+                return reval;
+            }
+            return reval;
+        }
+
+        public static int ObjToInt(this object thisValue, int errorValue)
+        {
+            if (thisValue != null && thisValue != DBNull.Value && int.TryParse(thisValue.ToString(), out int reval))
+            {
+                return reval;
+            }
+            return errorValue;
+        }
+
+        #endregion Convert To Int
+
+        #region Convert To Money
+
+        public static double ObjToMoney(this object thisValue)
+        {
+            if (thisValue != null && thisValue != DBNull.Value && double.TryParse(thisValue.ToString(), out double reval))
+            {
+                return reval;
+            }
+            return 0;
+        }
+
+        public static double ObjToMoney(this object thisValue, double errorValue)
+        {
+            if (thisValue != null && thisValue != DBNull.Value && double.TryParse(thisValue.ToString(), out double reval))
+            {
+                return reval;
+            }
+            return errorValue;
+        }
+
+        #endregion Convert To Money
+
+        #region Convert To String
+
+        public static string ObjToString(this object thisValue)
+        {
+            if (thisValue != null) return thisValue.ToString().Trim();
+            return "";
+        }
+
+        public static string ObjToString(this object thisValue, string errorValue)
+        {
+            if (thisValue != null) return thisValue.ToString().Trim();
+            return errorValue;
+        }
+
+        #endregion Convert To String
+
+        #region Convert To Decimal
+
+        public static decimal ObjToDecimal(this object thisValue)
+        {
+            if (thisValue != null && thisValue != DBNull.Value && decimal.TryParse(thisValue.ToString(), out decimal reval))
+            {
+                return reval;
+            }
+            return 0;
+        }
+
+        public static decimal ObjToDecimal(this object thisValue, decimal errorValue)
+        {
+            if (thisValue != null && thisValue != DBNull.Value && decimal.TryParse(thisValue.ToString(), out decimal reval))
+            {
+                return reval;
+            }
+            return errorValue;
+        }
+
+        #endregion Convert To Decimal
+
+        #region Convert To Date
+
+        public static DateTime ObjToDate(this object thisValue)
+        {
+            DateTime reval = DateTime.MinValue;
+            if (thisValue != null && thisValue != DBNull.Value && DateTime.TryParse(thisValue.ToString(), out reval))
+            {
+                reval = Convert.ToDateTime(thisValue);
+            }
+            return reval;
+        }
+
+        public static DateTime ObjToDate(this object thisValue, DateTime errorValue)
+        {
+            if (thisValue != null && thisValue != DBNull.Value && DateTime.TryParse(thisValue.ToString(), out DateTime reval))
+            {
+                return reval;
+            }
+            return errorValue;
+        }
+
+        #endregion Convert To Date
+
+        #region Convert To Boolean
+
+        public static bool ObjToBool(this object thisValue)
+        {
+            bool reval = false;
+            if (thisValue != null && thisValue != DBNull.Value && bool.TryParse(thisValue.ToString(), out reval))
+            {
+                return reval;
+            }
+            return reval;
+        }
+
+        #endregion Convert To Boolean
+    }
+}
